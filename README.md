09d75ce68b40ccd776c461064b195ee2e328161a
Andrew O <andrew.t.olguin@gmail.com>
2022-10-14 13:56:33 +0000

    First commoit

---
+++ README.md
@@ -0,0 +1,3 @@
+#toy-pipe
+
+A little experiment exploring how to use linux named pipes
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <array>
+
+int main(int argc, char *argv[]){
+    std::cout << "Hello World! I'm a C++ program!" << std::endl;
+
+    return 0;
+}
